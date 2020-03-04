b0860242a40da0f7664a920e1ae797d6729369a6
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-03-04 15:58:01 +0300

    add BlogItem

---
+++ src/components/blogs/BlogItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BlogItem = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content gret-text text-darken-3">
+        <span className="card-title">Название поста</span>
+        <p>Большой текст поста</p>
+        <p className="grey-text">04.03.2019 15:46</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogItem;
--- src/components/blogs/BlogList.js
+++ src/components/blogs/BlogList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import BlogItem from './BlogItem';
 
 const BlogList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content gret-text text-darken-3">
-          <span className="card-title">Название поста</span>
-          <p>Большой текст поста</p>
-          <p className="grey-text">04.03.2019 15:46</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content gret-text text-darken-3">
-          <span className="card-title">Название поста</span>
-          <p>Большой текст поста</p>
-          <p className="grey-text">04.03.2019 15:46</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content gret-text text-darken-3">
-          <span className="card-title">Название поста</span>
-          <p>Большой текст поста</p>
-          <p className="grey-text">04.03.2019 15:46</p>
-        </div>
-      </div>
+      <BlogItem />
+      <BlogItem />
+      <BlogItem />
+      <BlogItem />
     </div>
   );
 };
