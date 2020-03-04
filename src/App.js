5679ad971796b12a5cecdb176cb74bb60bd59e75
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-03-04 16:05:32 +0300

    add BlogDetail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import BlogDetail from './components/blogs/BlogDetail';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path="/" component={Dashboard} />
+        <Route path="/post/:id" component={BlogDetail} />
         <Redirect to="/" />
       </Switch>
     </div>
---
+++ src/components/blogs/BlogDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const BlogDetail = props => {
+  const id = props.match.params.id;
+
+  return (
+    <div className="container section project-detail">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Название поста - {id}</span>
+          <p>Большой текст поста</p>
+        </div>
+        <div className="card-action gret lighten-4 grey-text">
+          <div>Создал Sergey Startsev</div>
+          <div>04.03.2019 16:02</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetail;
