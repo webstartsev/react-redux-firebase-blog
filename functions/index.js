21bdd534ce4bf5364755c6c4c26381b8db1e26c7
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-03-05 15:48:00 +0300

    add postCreated firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+};
+
+exports.postCreated = functions.firestore.document('/posts/{postId}').onCreate(doc => {
+  const post = doc.data();
+  const notification = {
+    content: 'Добавлен новый пост',
+    user: `${post.authorFirstName} ${post.authorLastName}`,
+    time: admin.firestore.FieldValue.serverTimestamp()
+  };
+
+  return createNotification(notification);
+});
